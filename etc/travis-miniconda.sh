a4c54abf96e4531078edfbfd0911d083a569c657
C. E. Ball <ceball@gmail.com>
2017-11-29 12:23:23 +0000

    Initial website from docs branch, v1.0.0-2-gd263b36.

--- etc/travis-miniconda.sh
+++
@@ -1,9 +0,0 @@
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh;
-else
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
-fi
-bash miniconda.sh -b -p $HOME/miniconda
-export PATH="$HOME/miniconda/bin:$PATH"
-conda config --set always_yes yes --set changeps1 no
-conda update conda
--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='colorcet',
-      version='1.0.0',
-      description='Collection of perceptually uniform colormaps',
-      url='http://github.com/bokeh/colorcet',
-      maintainer='James A. Bednar',
-      maintainer_email='jbednar@continuum.io',
-      packages=['colorcet'])
