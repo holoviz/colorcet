17fae328e463733ef9b7e5718f3a521f435e5769
Julia Signell <jsignell@gmail.com>
2019-02-08 14:47:56 -0500

    Minor fixes

--- dodo.py
+++ dodo.py
@@ -2,3 +2,10 @@ import os
 if "PYCTDEV_ECOSYSTEM" not in os.environ:
     os.environ["PYCTDEV_ECOSYSTEM"] = "conda"
 from pyctdev import * # noqa: api
+
+
+def task_build_website():
+    return {'actions': [
+        "nbsite generate-rst --org pyviz --project-name colorcet --repo colorcet --offset 0"
+        "nbsite build --what=html --output=builtdocs"
+    ]}
