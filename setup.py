35f4a0e4f26afe7a243a12217465b6e42041c4d9
Julia Signell <jsignell@gmail.com>
2019-02-11 16:51:53 -0500

    Moving from .cfg to .py

--- setup.py
+++ setup.py
@@ -1,17 +1,97 @@
-from setuptools import setup
-import os, sys, shutil
+import os
+import sys
+import json
+import hashlib
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
 
 import pyct.build
 
+def get_setup_version(reponame):
+    """
+    Helper to get the current version from either git describe or the
+    .version file (if available).
+    """
+    import json
+    basepath = os.path.split(__file__)[0]
+    version_file_path = os.path.join(basepath, reponame, '.version')
+    try:
+        from param import version
+    except:
+        version = None
+    if version is not None:
+        return version.Version.setup_version(basepath, reponame, archive_commit="$Format:%h$")
+    else:
+        print("WARNING: param>=1.6.0 unavailable. If you are installing a package, this warning can safely be ignored. If you are creating a package or otherwise operating in a git repository, you should install param>=1.6.0.")
+        return json.load(open(version_file_path, 'r'))['version_string']
+
+
+########## dependencies ##########
+
+install_requires = [
+    'param >=1.7.0',
+]
+
+examples = [
+    'numpy',
+    'holoviews'
+]
+
+extras_require = {
+    'tests': [
+        'flake8',
+        'nbsmoke >=0.2.6',
+        'pytest >=2.8.5',
+    ],
+    'examples': examples,
+    'doc': examples + [
+        'nbsite',
+        'sphinx_ioam_theme',
+    ]
+}
+
+extras_require['all'] = sorted(set(sum(extras_require.values(), [])))
+
+setup_args = dict(
+    name='colorcet',
+    description='Collection of perceptually uniform colormaps',
+    version=get_setup_version('colorcet'),
+    long_description='README.md',
+    long_description_type='text/markdown',
+    license_file='LICENSE.txt',
+    license='CC-BY License',
+    classifiers=[
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Development Status :: 5 - Production/Stable",
+    ],
+    author="James A. Bednar",
+    author_email="jbednar@anaconda.com",
+    maintainer="James A. Bednar",
+    maintainer_email="jbednar@anaconda.com",
+    url="https://colorcet.pyviz.org",
+    project_urls = {
+        "Bug Tracker": "http://github.com/pyviz/colorcet/issues",
+        "Documentation": "https://colorcet.pyviz.org",
+        "Source Code": "http://github.com/pyviz/colorcet",
+    },
+    include_package_data=True,
+    packages=find_packages(),
+    python_requires=">=2.7",
+    install_requires=install_requires,
+    extras_require=extras_require,
+)
+
+
 if __name__=="__main__":
-    # TODO: hope to eliminate the examples handling from here
-    # (i.e. all lines except setup()), moving it to pyct
     example_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                 'colorcet','examples')
     if 'develop' not in sys.argv:
         pyct.build.examples(example_path, __file__, force=True)
 
-    setup()
-
-    if os.path.isdir(example_path):
-        shutil.rmtree(example_path)
+    setup(**setup_args)
