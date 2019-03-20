eda599c92b0ffb09cd5343f2381ffc5c8fd6ccdb
Julia Signell <jsignell@gmail.com>
2019-03-20 12:39:41 -0400

    Adding some glasbey tests

--- colorcet/tests/test_bokeh.py
+++ colorcet/tests/test_bokeh.py
@@ -2,11 +2,17 @@ import pytest  # noqa
 import colorcet as cc
 
 def test_bokeh_palettes_available():
-    assert len(cc.palette.items()) == 72
-    assert len(cc.palette_n.items()) == 21
+    assert len(cc.palette.items()) == 81
+    assert len(cc.palette_n.items()) == 24
 
 def test_bokeh_palette_is_a_list():
     assert isinstance(cc.blues, list)
     assert len(cc.blues) == 256
     assert cc.blues[0] == '#f0f0f0'
     assert cc.blues[-1] == '#3a7bb1'
+
+def test_bokeh_palette_glasbey_do_not_start_with_bw():
+    for cmap in [cc.glasbey, cc.Category10, cc.Category20]:
+        assert isinstance(cmap, list)
+        assert len(cmap) == 256
+        assert {cmap[0], cmap[1]} != {'#00000', '#ffffff'}
--- colorcet/tests/test_matplotlib.py
+++ colorcet/tests/test_matplotlib.py
@@ -3,8 +3,8 @@ import colorcet as cc
 
 def test_matplotlib_colormaps_available():
     pytest.importorskip('matplotlib')
-    assert len(cc.cm.items()) == 144
-    assert len(cc.cm_n.items()) == 42
+    assert len(cc.cm.items()) == 162
+    assert len(cc.cm_n.items()) == 48
 
 
 @pytest.mark.mpl_image_compare
@@ -14,4 +14,14 @@ def test_matplotlib():
     import matplotlib.pyplot as plt
     xs, _ = np.meshgrid(np.linspace(0, 1, 80), np.linspace(0, 1, 10))
     fig = plt.imshow(xs, cmap=cc.cm.colorwheel).get_figure()
+    return fig
+
+
+@pytest.mark.mpl_image_compare
+def test_matplotlib_glasbey():
+    pytest.importorskip('matplotlib')
+    import numpy as np
+    import matplotlib.pyplot as plt
+    xs, _ = np.meshgrid(np.linspace(0, 1, 256), np.linspace(0, 1, 10))
+    fig = plt.imshow(xs, cmap=cc.cm.glasbey).get_figure()
     return fig
