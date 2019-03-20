ca257f811dd895581ff124a7ac1db183cfbfd245
Julia Signell <jsignell@gmail.com>
2019-03-20 09:25:57 -0400

    Finishing adding assets

--- assets/CET_to_py.py
+++ assets/CET_to_py.py
@@ -1,11 +1,14 @@
 """
 Generate Python versions for each of the colormaps provided in
 http://peterkovesi.com/projects/colourmaps/CETperceptual_csv_0_1.zip
+
+Adding in those in Glasbey which were created using: https://github.com/taketwo/glasbey.git
+see https://github.com/pyviz/colorcet/issues/11 for more details
 """
 
 import os, os.path, csv
 
-path = 'CETperceptual_csv_0_1'
+paths = ['CETperceptual_csv_0_1', 'Glasbey']
 output_file = '../colorcet/__init__.py'
 header = '''\
 """
@@ -32,15 +35,22 @@ All colormaps are named using Peter Kovesi\'s naming scheme:
 
 <category>_<huesequence>_<lightnessrange>_c<meanchroma>[_s<colorshift>_[r<ifreversed>]]
 
-but some have shorter, more convenient aliases, some of which are 
+but some have shorter, more convenient aliases, some of which are
 inspired by Matplotlib colormaps of the same name and others
-based on the qualitative appearance.  The colormaps with 
+based on the qualitative appearance.  The colormaps with
 shorter names tend to be the most useful subset, and for
 cases like automatic population of a GUI widget these
 colormaps are provided as a separate subset:
 
   palette_n['name'] or palette_n.name
   cm_n['name'] or cm_n.name
+
+Also included are some sets of 256 Glasbey colors. These are available via the
+same methods described above and are named:
+
+  glasbey_<starting_palette>_start_[_no_black<if_no_black>]
+
+The no_black versions are aliased to their starting palette names.
 """
 
 __version__ = '1.0.0'
@@ -66,7 +76,7 @@ except:
     LinearSegmentedColormap.from_list=lambda n,c,N: None
 
 def rgb_to_hex(r,g,b):
-    return '#%02x%02x%02x' % (r,g,b) 
+    return '#%02x%02x%02x' % (r,g,b)
 
 def bokeh_palette(name,colorlist):
     palette[name] = [rgb_to_hex(int(r*255),int(g*255),int(b*255)) for r,g,b in colorlist]
@@ -106,40 +116,44 @@ aliases = dict(
   linear_green_5_95_c69                  = 'kgy',
   linear_grey_0_100_c0                   = 'gray',     #mpl
   linear_grey_10_95_c0                   = 'dimgray',
-  linear_kryw_0_100_c71                  = 'fire', 
+  linear_kryw_0_100_c71                  = 'fire',
   linear_ternary_blue_0_44_c57           = 'kb',
   linear_ternary_green_0_46_c42          = 'kg',
   linear_ternary_red_0_50_c52            = 'kr',
   rainbow_bgyr_35_85_c73                 = 'rainbow',
+  glasbey_white_start_no_black           = 'Category',
+  glasbey_category10_start_no_black     = 'Category10',  #bokeh
+  glasbey_category20_start_no_black     = 'Category20',  #bokeh
 )
 
 
 with open(output_file, "w") as output:
     output.write(header)
-    for filename in os.listdir(path):
-        if filename.endswith(".csv"):
-            base = filename[:-4].replace("-","_").replace("_n256","")
-            output.write("\n\n"+base+" = [\\\n")
-            with open(os.path.join(path,filename),'r') as csvfile:
-                reader = csv.reader(csvfile)
-                for row in reader:
-                    output.write("["+', '.join(row)+"],\n")
-            output.write("]\n")
-            output.write("b_{0} = bokeh_palette('{0}',{0})\n".format(base))
-            output.write("m_{0} = mpl_cm('{0}',{0})\n".format(base))
-            output.write("m_{0}_r = mpl_cm('{0}_r',list(reversed({0})))\n".format(base))
-            if base in aliases:
-                alias = aliases[base]
-                output.write("{0} = b_{1}\n".format(alias,base))
-                output.write("m_{0} = m_{1}\n".format(alias,base))
-                output.write("m_{0}_r = m_{1}_r\n".format(alias,base))
-                output.write("palette['{0}'] = b_{1}\n".format(alias,base))
-                output.write("palette_n['{0}'] = b_{1}\n".format(alias,base))
-                output.write("cm['{0}'] = m_{1}\n".format(alias,base))
-                output.write("cm['{0}_r'] = m_{1}_r\n".format(alias,base))
-                output.write("cm_n['{0}'] = mpl_cm('{0}',{1})\n".format(alias,base))
-                output.write("cm_n['{0}_r'] = mpl_cm('{0}_r',list(reversed({1})))\n".format(alias,base))
-                output.write("register_cmap('cet_{0}',m_{1})\n".format(alias,base))
-                output.write("register_cmap('cet_{0}_r',m_{1}_r)\n".format(alias,base))
-            output.write("\n\n")
-    output.write(footer)     
+    for path in paths:
+      for filename in os.listdir(path):
+          if filename.endswith(".csv"):
+              base = filename[:-4].replace("-","_").replace("_n256","")
+              output.write("\n\n"+base+" = [\\\n")
+              with open(os.path.join(path,filename),'r') as csvfile:
+                  reader = csv.reader(csvfile)
+                  for row in reader:
+                      output.write("["+', '.join(row)+"],\n")
+              output.write("]\n")
+              output.write("b_{0} = bokeh_palette('{0}',{0})\n".format(base))
+              output.write("m_{0} = mpl_cm('{0}',{0})\n".format(base))
+              output.write("m_{0}_r = mpl_cm('{0}_r',list(reversed({0})))\n".format(base))
+              if base in aliases:
+                  alias = aliases[base]
+                  output.write("{0} = b_{1}\n".format(alias,base))
+                  output.write("m_{0} = m_{1}\n".format(alias,base))
+                  output.write("m_{0}_r = m_{1}_r\n".format(alias,base))
+                  output.write("palette['{0}'] = b_{1}\n".format(alias,base))
+                  output.write("palette_n['{0}'] = b_{1}\n".format(alias,base))
+                  output.write("cm['{0}'] = m_{1}\n".format(alias,base))
+                  output.write("cm['{0}_r'] = m_{1}_r\n".format(alias,base))
+                  output.write("cm_n['{0}'] = mpl_cm('{0}',{1})\n".format(alias,base))
+                  output.write("cm_n['{0}_r'] = mpl_cm('{0}_r',list(reversed({1})))\n".format(alias,base))
+                  output.write("register_cmap('cet_{0}',m_{1})\n".format(alias,base))
+                  output.write("register_cmap('cet_{0}_r',m_{1}_r)\n".format(alias,base))
+              output.write("\n\n")
+    output.write(footer)
