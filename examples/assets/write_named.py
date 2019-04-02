2258f5e165c15c6bcb9cc1c37d73b35e45ee8fe2
Julia Signell <jsignell@gmail.com>
2019-04-02 16:04:47 -0500

    Making things faster

--- examples/assets/write_named.py
+++ examples/assets/write_named.py
@@ -13,7 +13,7 @@ misc_n = sorted([k for k in cc.aliases if k not in cat_n + diverging_n + linear_
 
 diverging_col = pn.Column('#Diverging', sine_combs(*diverging_n, width=400, height=150).opts(toolbar=None))
 linear_col = pn.Column('#Linear', sine_combs(*linear_n, width=400, height=150).opts(toolbar=None))
-cat_col = pn.Column('#Categorical', swatches(*cat_n, width=400, height=150).opts(toolbar=None))
+cat_col = pn.Column('#Categorical', swatches(*cat_n, width=400, height=150, cols=1).opts(toolbar=None))
 misc_col = pn.Column('#Misc', sine_combs(*misc_n, width=400, height=150).opts(toolbar=None))
 
 all_named = pn.Row(
