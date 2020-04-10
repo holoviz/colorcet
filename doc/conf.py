25e99e1bbd8768c19af79bc8c598de129e4e68f9
Kenneth Bowen <kebowen730@gmail.com>
2020-04-10 15:50:47 -0500

    update the html theme to use sphinx_holoviz_theme

--- doc/conf.py
+++ doc/conf.py
@@ -13,7 +13,7 @@ version = release = colorcet.__version__
 nbbuild_cell_timeout = 10000
 
 html_static_path += ['_static']
-html_theme = 'sphinx_ioam_theme'
+html_theme = 'sphinx_holoviz_theme'
 # logo file etc should be in html_static_path, e.g. _static
 
 templates_path = ['_templates']
