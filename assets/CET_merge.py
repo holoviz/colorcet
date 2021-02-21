b50dc2ebad515dd8ce812f8f6acedfd38969883c
Randall Pittman <randallpittman@outlook.com>
2021-02-20 22:45:33 -0800

    CET_merge.py - fix merging of dict of lists, and fix mapping sort

--- assets/CET_merge.py
+++ assets/CET_merge.py
@@ -4,7 +4,7 @@
 # 3. After running script:
 #    a. Add the new mapsdir to the `paths` list in CET_to_py.py.
 #    b. Replace the existing `aliases` and `mapping` in CET_to_py.py with those printed by this script.
-
+import re
 from CET_updates import new_aliases, new_mappings, new_mapsdir
 
 # paste `aliases` from CET_to_py.py here
@@ -13,11 +13,15 @@ aliases = dict()
 # paste `mapping` from CET_to_py.py here
 mapping = {}
 
+def merge_dicts_of_lists(orig, new):
+    for k, v in new.items():
+        orig[k] = orig.get(k, []) + v
 
-aliases.update(new_aliases)
+
+merge_dicts_of_lists(aliases, new_aliases)
 mapping.update(new_mappings)
 
-def print_dict(name, d, braces=False, tabs = 0, evenspace=False):
+def print_dict(name, d, braces=False, tabs = 0, evenspace=False, sortfn=lambda k: k):
     if evenspace:
         k_maxlen = 0
         for k in d:
@@ -31,11 +35,15 @@ def print_dict(name, d, braces=False, tabs = 0, evenspace=False):
     s4 = ' '*4
     tabs = s4*tabs
     print(tabs + "{0} = {1}".format(name, start))
-    for k in sorted(d, key=lambda k: k):
+    for k in sorted(d, key=sortfn):
         val = "'{0}'".format(d[k]) if isinstance(d[k], str) else d[k]
         print(tabs + s4 + fmt.format(k, val))
     print(tabs + end)
 
+def CET_sortfn(k):
+    prefix, num, suffix = re.match("(CET-\D+)(\d+)(\D*)", k).groups()
+    return "{}{:02d}{}".format(prefix, int(num), suffix)
+
 print("new_mapsdir = '{0}'".format(new_mapsdir))
 print_dict('aliases', aliases, evenspace=True)
-print_dict('mapping', mapping, braces=True)
+print_dict('mapping', mapping, braces=True, sortfn=CET_sortfn)
