0951b311d7c643fb6c5b27db03bb5ba7a44f99eb
James A. Bednar <jbednar@continuum.io>
2021-02-23 15:56:34 -0600

    Fixed axis linking and balanced columns

--- examples/assets/write_named.py
+++ examples/assets/write_named.py
@@ -7,20 +7,20 @@ import panel as pn
 hv.extension('bokeh')
 
 diverging_n = cc.all_original_names(group='diverging', only_aliased=True)
-linear_n = cc.all_original_names(group='linear', not_group='diverging', only_aliased=True)
-cat_n = cc.all_original_names(group='glasbey', only_aliased=True)
-misc_n = sorted([k for k in cc.aliases if k not in cat_n + diverging_n + linear_n])
+linear_n    = cc.all_original_names(group='linear',    only_aliased=True, not_group='diverging')
+cat_n       = cc.all_original_names(group='glasbey',   only_aliased=True)
+misc_n      = sorted([k for k in cc.aliases if k not in cat_n + diverging_n + linear_n])
 
-diverging_col = pn.Column('#Diverging', sine_combs(*diverging_n, width=400, height=150).opts(toolbar=None))
-linear_col = pn.Column('#Linear', sine_combs(*linear_n, width=400, height=150).opts(toolbar=None))
-cat_col = pn.Column('#Categorical', swatches(*cat_n, width=400, height=150, cols=1).opts(toolbar=None))
-misc_col = pn.Column('#Misc', sine_combs(*misc_n, width=400, height=150).opts(toolbar=None))
+def pane(fn, cmaps):
+    return pn.panel(fn(*cmaps, width=400, height=150, cols=1).opts(toolbar=None), linked_axes=False)
 
-all_named = pn.Row(
-    linear_col, pn.Spacer(width=100),
-    pn.Column(
-        diverging_col, pn.Spacer(height=102),
-        cat_col, pn.Spacer(height=102),
-        misc_col))
+diverging_col = pn.Column('#Diverging',   pane(sine_combs, diverging_n))
+linear_col    = pn.Column('#Linear',      pane(sine_combs, linear_n))
+cat_col       = pn.Column('#Categorical', pane(swatches,   cat_n))
+misc_col      = pn.Column('#Misc',        pane(sine_combs, misc_n))
+
+all_named = pn.Row(pn.Column(linear_col,    pn.Spacer(height=102), cat_col),
+                   pn.Spacer(width=100),
+                   pn.Column(diverging_col, pn.Spacer(height=102), misc_col))
 
 all_named.save('./images/named.png')
