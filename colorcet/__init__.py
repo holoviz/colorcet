9a974a66aadd90a6b8207bc4b7178fecaba36923
maximlt <mliquet@anaconda.com>
2024-02-14 08:40:01 +0100

    remove code

--- colorcet/__init__.py
+++ colorcet/__init__.py
@@ -40,45 +40,28 @@ same methods described above and are named:
 Some of the Glasbey sets are aliased to short names as explained in the User Guide.
 """
 
-import os
-
 from collections import OrderedDict
 from itertools import chain
 
 # Define '__version__'
 try:
-    # For performance reasons on imports, avoid importing setuptools_scm
-    # if not in a .git folder
-    if os.path.exists(os.path.join(os.path.dirname(__file__), "..", ".git")):
-        # If setuptools_scm is installed (e.g. in a development environment with
-        # an editable install), then use it to determine the version dynamically.
-        from setuptools_scm import get_version
+    # __version__ was added in _version in setuptools-scm 7.0.0, we rely on
+    # the hopefully stable version variable.
+    from ._version import version as __version__
+except (ModuleNotFoundError, ImportError):
+    # Either _version doesn't exist (ModuleNotFoundError) or version isn't
+    # in _version (ImportError). ModuleNotFoundError is a subclass of
+    # ImportError, let's be explicit anyway.
 
-        # This will fail with LookupError if the package is not installed in
-        # editable mode or if Git is not installed.
-        __version__ = get_version(root="..", relative_to=__file__)
-    else:
-        raise FileNotFoundError
-except (ImportError, LookupError, FileNotFoundError):
-    # As a fallback, use the version that is hard-coded in the file.
+    # Try something else:
+    from importlib.metadata import version as mversion, PackageNotFoundError
+
     try:
-        # __version__ was added in _version in setuptools-scm 7.0.0, we rely on
-        # the hopefully stable version variable.
-        from ._version import version as __version__
-    except (ModuleNotFoundError, ImportError):
-        # Either _version doesn't exist (ModuleNotFoundError) or version isn't
-        # in _version (ImportError). ModuleNotFoundError is a subclass of
-        # ImportError, let's be explicit anyway.
-
-        # Try something else:
-        from importlib.metadata import version as mversion, PackageNotFoundError
-
-        try:
-            __version__ = mversion("colorcet")
-        except PackageNotFoundError:
-            # The user is probably trying to run this without having installed
-            # the package.
-            __version__ = "0.0.0+unknown"
+        __version__ = mversion("colorcet")
+    except PackageNotFoundError:
+        # The user is probably trying to run this without having installed
+        # the package.
+        __version__ = "0.0.0+unknown"
 
 
 class AttrODict(OrderedDict):
